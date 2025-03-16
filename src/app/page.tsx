46ff5d1065f21ffa057cd9ac2a0d6940a9c0e7d3
Neel Manglani <neel738@hotmail.com>
2025-03-17 00:17:53 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@
 import { Experience } from "@/components/experience";
 import Skills from "@/components/features-1";
 import HeroSection from "@/components/hero-section";
-import { config } from "@/config";
-import { motion } from "framer-motion";
+
 
 export default function Home() {
   return (
