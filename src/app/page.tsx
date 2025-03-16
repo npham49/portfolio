79b1804637f18560f8231c7020cb3633422674b0
Neel Manglani <neel738@hotmail.com>
2025-03-17 00:02:06 +0100

    hero added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,12 @@
 "use client";
+import HeroSection from "@/components/hero-section";
 import { config } from "@/config";
 import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center  ">
-      <div className="flex flex-col items-center justify-center ">
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="flex flex-col items-center justify-center h-screen"
-        >
-          <h1 className="text-4xl font-bold font-gilda gilda-text-bg">
-            {config.hero.title}
-          </h1>
-          <h2 className="text-2xl font-bold font-gilda gilda-text-bg">
-            {config.hero.subtitle}
-          </h2>
-        </motion.div>
-      </div>
-      <div
-        className="flex flex-col items-center justify-center "
-        id="experience"
-      >
-        <motion.div
-          initial={{ opacity: 0, y: 100 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="flex flex-col items-center justify-center h-screen"
-        >
-          <h1 className="text-4xl font-bold">Experience</h1>
-        </motion.div>
-      </div>
+        <HeroSection />
     </div>
   );
 }
